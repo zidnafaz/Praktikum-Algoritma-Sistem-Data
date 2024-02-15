30417a18f588826493ba4a86276316319a73ad99
zidnafaz <zidnafaza27@gmail.com>
2024-02-15 12:47:58 +0700

    Array

--- Jobsheet 01/src/Array_18.java
+++ Jobsheet 01/src/Array_18.java
@@ -1,7 +1,31 @@
 package src;
 
+import java.util.Scanner;
+
 public class Array_18 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("=============================");
+        System.out.print("Masukkan NIM  : ");
+        String nim = scanner.nextLine();
+        System.out.println("=============================");
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+
+        if (n < 10) {
+            n += 10;
+        }
+        
+        System.out.println("Nilai n       : " + n);
+
+        for (int i = 1; i <= n; i++) {
+            if (i != 6 && i != 10) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+        }
     }
 }

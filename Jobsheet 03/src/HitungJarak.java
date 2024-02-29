fd8450a9408b143d0092f745e82fe2e1df184b30
zidnafaz <zidnafaza27@gmail.com>
2024-02-29 17:59:31 +0700

    Perbaikan Tugas 2

--- Jobsheet 03/src/HitungJarak.java
+++ Jobsheet 03/src/HitungJarak.java
@@ -1,5 +1,5 @@
 public class HitungJarak {
-    
+
     public String alamatPenerima;
     public String alamatPengirim;
     public int jarak = 0;
@@ -7,39 +7,43 @@ public class HitungJarak {
     public HitungJarak(String alamatPenerima, String alamatPengirim) {
 
         this.alamatPenerima = alamatPenerima;
-        this.alamatPengirim = alamatPengirim;        
+        this.alamatPengirim = alamatPengirim;
 
     }
 
-    public int Jarak (String alamatPenerima, String alamatPengirim) {
+    public void hitungJarak() {
+        jarak = Jarak(alamatPenerima, alamatPengirim);
+    }
+
+    public int Jarak(String alamatPenerima, String alamatPengirim) {
 
         if ((alamatPengirim.equalsIgnoreCase("Malang") && alamatPenerima.equalsIgnoreCase("Jakarta")) ||
                 (alamatPengirim.equalsIgnoreCase("Jakarta") && alamatPenerima.equalsIgnoreCase("Malang"))) {
-            jarak = 850;
+            return 850;
         } else if ((alamatPengirim.equalsIgnoreCase("Malang") && alamatPenerima.equalsIgnoreCase("Bandung")) ||
                 (alamatPengirim.equalsIgnoreCase("Bandung") && alamatPenerima.equalsIgnoreCase("Malang"))) {
-            jarak = 700;
+            return 700;
         } else if ((alamatPengirim.equalsIgnoreCase("Malang") && alamatPenerima.equalsIgnoreCase("Surabaya")) ||
                 (alamatPengirim.equalsIgnoreCase("Surabaya") && alamatPenerima.equalsIgnoreCase("Malang"))) {
-            jarak = 40;
+            return 40;
         } else if ((alamatPengirim.equalsIgnoreCase("Malang") && alamatPenerima.equalsIgnoreCase("Semarang")) ||
                 (alamatPengirim.equalsIgnoreCase("Semarang") && alamatPenerima.equalsIgnoreCase("Malang"))) {
-            jarak = 400;
+            return 400;
         } else if ((alamatPengirim.equalsIgnoreCase("Malang") && alamatPenerima.equalsIgnoreCase("Serang")) ||
                 (alamatPengirim.equalsIgnoreCase("Serang") && alamatPenerima.equalsIgnoreCase("Malang"))) {
-            jarak = 900;
+            return 900;
         } else if ((alamatPengirim.equalsIgnoreCase("Surabaya") && alamatPenerima.equalsIgnoreCase("Solo")) ||
                 (alamatPengirim.equalsIgnoreCase("Solo") && alamatPenerima.equalsIgnoreCase("Surabaya"))) {
-            jarak = 250;
+            return 250;
         } else {
             System.out.println("Pengiriman tidak tersedia. Masukkan alamat dengan benar.\n");
-            
+            return 0;
         }
-
-        return jarak;
     }
-    
+
     public void tampilkanJarak() {
-        System.out.println("Jarak pengiriman anda adalah " + jarak + " km");
+        System.out.println("Dari     : " + alamatPengirim);
+        System.out.println("Ke       : " + alamatPenerima);
+        System.out.println("Jarak    : " + jarak);
     }
 }
--- Jobsheet 03/src/MainHitungJarak.java
+++ Jobsheet 03/src/MainHitungJarak.java
@@ -4,16 +4,29 @@ public class MainHitungJarak {
     public static void main(String[] args) {
         Scanner input18 = new Scanner(System.in);
 
-        String alamatPenerima;
-        String alamatPengirim;
+        HitungJarak[] arrayOfHitungJarak = new HitungJarak[3];
 
-        System.out.print("Masukkan alamat Pengirim    : ");
-        alamatPengirim = input18.nextLine();
-        System.out.print("Masukkan alamat Penerima    : ");
-        alamatPenerima = input18.nextLine();
+        String alamatPenerima = "";
+        String alamatPengirim = "";
+        int jarak;
 
-        HitungJarak HitungJarak1 = new HitungJarak(alamatPenerima, alamatPengirim);
-        HitungJarak1.Jarak(alamatPenerima, alamatPengirim);
-        HitungJarak1.tampilkanJarak();
+        for (int i = 0; i < arrayOfHitungJarak.length; i++) {
+
+            System.out.println("\nData Pengiriman Ke-" + (i + 1));
+            
+            System.out.print("Masukkan alamat Pengirim    : ");
+            alamatPengirim = input18.nextLine();
+            System.out.print("Masukkan alamat Penerima    : ");
+            alamatPenerima = input18.nextLine();
+
+            arrayOfHitungJarak[i] = new HitungJarak(alamatPenerima, alamatPengirim);
+            arrayOfHitungJarak[i].hitungJarak();
+
+        }
+
+        for (int i = 0; i < arrayOfHitungJarak.length; i++) {
+            System.out.println("\nData Pengiriman Ke-" + (i + 1));
+            arrayOfHitungJarak[i].tampilkanJarak();
+        }
     }
 }

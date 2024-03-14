e5e2b2f60b9d35db5584506b65ff801cb76dda11
zidnafaz <zidnafaza27@gmail.com>
2024-03-14 09:13:45 +0700

    Percobaan 1

--- Jobsheet 05/src/MainFaktorial.java
+++ Jobsheet 05/src/MainFaktorial.java
@@ -4,7 +4,7 @@ public class MainFaktorial {
     public static void main(String[] args) {
         Scanner input18 = new Scanner(System.in);
 
-        System.out.println("========================================");
+        System.out.println("============================================================");
         System.out.print("Masukkan jumlah elemen yang ingin dihitung : ");
         int elemen = input18.nextInt();
 
@@ -16,21 +16,21 @@ public class MainFaktorial {
             fk[i].nilai = input18.nextInt();
         }
 
-        System.out.println("========================================");
+        System.out.println("============================================================");
         System.out.println("Hasil Faktorial dengan Brute Force");
 
         for (int i = 0; i < elemen; i++) {
             System.out.println("Faktorial dari nilai " + fk[i].nilai + " adalah : " + fk[i].faktorialBF(fk[i].nilai));
         }
 
-        System.out.println("========================================");
+        System.out.println("============================================================");
         System.out.println("Hasil Faktorial dengan Divide and Conquer");
 
         for (int i = 0; i < elemen; i++) {
             System.out.println("Faktorial dari nilai " + fk[i].nilai + " adalah : " + fk[i].faktorialDC(fk[i].nilai));
         }
 
-        System.out.println("========================================");
+        System.out.println("============================================================");
 
     }
 }

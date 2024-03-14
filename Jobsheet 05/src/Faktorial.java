db18814349a349b6728447dae88b4547280a5b45
zidnafaz <zidnafaza27@gmail.com>
2024-03-14 10:58:58 +0700

    Pertanyaan dan Modifikasi Percobaan 1

--- Jobsheet 05/src/Faktorial.java
+++ Jobsheet 05/src/Faktorial.java
@@ -1,27 +1,71 @@
+import java.util.concurrent.TimeUnit;
+
 public class Faktorial {
 
     public int nilai;
 
-    public int faktorialBF(int n) {
+    public int faktorialBFRekursif(int n) {        
 
-        int fakto = 1;
+        // SEBELUM PERTANYAAN
 
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+        // int fakto = 1;
+
+        // for (int i = 1; i <= n; i++) {
+        //     fakto = fakto * i;
+        // }
+
+        // return fakto;
+
+        // SESUDAH PERTANYAAN
+
+        if (n == 0 || n == 1) {
+
+            return 1;
+
+        } else {
+
+            return n * faktorialBFRekursif(n - 1);
+            
         }
 
-        return fakto;
+    }
+
+    public int faktorialBF(int n) {
+        long startTime = System.nanoTime();
+
+        int result = faktorialBFRekursif(n);
+
+        long endTime = System.nanoTime();
+        long durationInNano = endTime - startTime;
+        System.out.println("Waktu eksekusi : " + durationInNano + " ms");
+
+        return result;
+    }
+
+    public int faktorialDCRekursif(int n) {
+
+        if (n==1) {
+
+            return 1;
+
+        } else {
+
+            int fakto = n * faktorialDCRekursif(n-1);
+            return fakto;
+
+        }
 
     }
 
     public int faktorialDC(int n) {
+        long startTime = System.nanoTime();
 
-        if (n==1) {
-            return 1;
-        } else {
-            int fakto = n * faktorialDC(n-1);
-            return fakto;
-        }
+        int result = faktorialDCRekursif(n);
 
+        long endTime = System.nanoTime();
+        long durationInNano = endTime - startTime;
+        System.out.println("Waktu eksekusi : " + durationInNano + " ms");
+
+        return result;
     }
 }

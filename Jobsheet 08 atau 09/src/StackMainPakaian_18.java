1457b5cb6d52259fd5b33dfd54045a082b6bd57e
zidnafaz <zidnafaza27@gmail.com>
2024-04-04 11:54:00 +0700

    Modifikasi Percobaan 2 Pertanyaan No 4

--- Jobsheet 08 atau 09/src/StackMainPakaian_18.java
+++ Jobsheet 08 atau 09/src/StackMainPakaian_18.java
@@ -1,42 +1,83 @@
 import java.util.Scanner;
 
 public class StackMainPakaian_18 {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner input18 = new Scanner(System.in);
+        Scanner input18String = new Scanner(System.in);
 
         StackPakaian_18 stack = new StackPakaian_18(5);
         char pilihan;
+        int pilihanInt = 0;
 
         do {
 
-            System.out.print("Jenis     : ");
-            String jenis = input18.nextLine();
-            System.out.print("Warna     : ");
-            String warna = input18.nextLine();
-            System.out.print("Merk      : ");
-            String merk = input18.nextLine();
-            System.out.print("Ukuran    : ");
-            String ukuran = input18.nextLine();
-            System.out.print("Harga     : ");
-            double harga = input18.nextDouble();
+            System.out.println("==============================");
+            System.out.println("            MENU");
+            System.out.println("==============================");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Print");
+            System.out.println("0. Exit");
+            System.out.println("==============================");
+            System.out.print("Masukkan pilihan : ");
+            pilihanInt = input18.nextInt();
 
-            Pakaian_18 pakaian = new Pakaian_18(jenis, warna, merk, ukuran, harga);
+            switch (pilihanInt) {
+                case 1:
+                    do {
+                        if (stack.isFull()) {
+                            System.out.println("Stack sudah penuh!");
+                            break;
+                        }
 
-            System.out.print("Apakah anda akan menambahkan data baru ke stack (y/n)?  : ");
-            pilihan = input18.next().charAt(0);
-            input18.nextLine();
+                        System.out.print("\nJenis     : ");
+                        String jenis = input18String.nextLine();
+                        System.out.print("Warna     : ");
+                        String warna = input18String.nextLine();
+                        System.out.print("Merk      : ");
+                        String merk = input18String.nextLine();
+                        System.out.print("Ukuran    : ");
+                        String ukuran = input18String.nextLine();
+                        System.out.print("Harga     : ");
+                        double harga = input18.nextDouble();
 
-            stack.push(pakaian);
+                        Pakaian_18 pakaian = new Pakaian_18(jenis, warna, merk, ukuran, harga);
 
-        } while (pilihan == 'y');
+                        System.out.print("Apakah anda akan menambahkan data baru ke stack (y/n)?  : ");
+                        pilihan = input18.next().charAt(0);
 
-        stack.print();
-        stack.pop();
-        stack.peek();
-        stack.print();
-    
+                        stack.push(pakaian);
+
+                    } while (pilihan == 'y');
+                    break;
+                case 2:
+                    System.out.println("==============================");
+                    stack.pop();
+                    break;
+                case 3:
+                    System.out.println("==============================");
+                    stack.peek();
+                    break;
+                case 4:
+                    System.out.println("==============================");
+                    stack.print();
+                    break;
+                case 0:
+                    System.out.println("==============================");
+                    System.out.println("    Keluar dari program.");
+                    System.out.println("==============================");
+                    break;
+                default:
+                    System.out.println("==============================");
+                    System.out.println(" Masukkan pilihan yang BENAR");
+                    break;
+            }
+
+        } while (pilihanInt != 0);
+
     }
 
 }

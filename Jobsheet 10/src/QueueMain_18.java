f37c16a4726cba7ec8a6356b26c036707e6c3d9c
zidnafaz <zidnafaza27@gmail.com>
2024-04-25 09:32:05 +0700

    Praktikum 1

---
+++ Jobsheet 10/src/QueueMain_18.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class QueueMain_18 {
+
+    public static void main(String[] args) {
+        Scanner input18 = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas queue  : ");
+        int n = input18.nextInt();
+
+        Queue_18 Q = new Queue_18(n);
+
+        int pilihan = 0;
+        do {
+            menu();
+            pilihan = input18.nextInt();
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan data baru  : ");
+                    int dataMasuk = input18.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan   : " + dataKeluar);
+                    }
+                    break;
+
+                case 3:
+                    Q.print();
+                    break;
+
+                case 4:
+                    Q.peek();
+                    break;
+
+                case 5:
+                    Q.clear();
+                    break;
+
+                default:
+                    break;
+            }
+        } while (pilihan != 6);
+    }
+
+    public static void menu() {
+        System.out.println("\nMasukkan operasi yang diinginkan");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("-------------------------------------------");
+    }
+
+}
